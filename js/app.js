ab900df47f4860c2065a3425dfcad714eb2b0dac
Adrian Grimm <usmcamgrimm@gmail.com>
2021-05-24 12:49:57 -0400

    Add home page content

---
+++ js/app.js
@@ -0,0 +1,10 @@
+document.getElementById('base').innerHTML= `
+  <section id="main">
+    <div class="welcome">
+      <p>
+        Managed Assets Portal
+      </p>
+      <img src="resources/logo.png" class="welcomeLogo" alt="SES logo" />
+    </div>
+  </section>
+`
