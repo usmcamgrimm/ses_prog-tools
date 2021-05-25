697fbf1c652f5e5fdd3fac30b37792fd20a5e2da
Adrian Grimm <usmcamgrimm@gmail.com>
2021-05-25 11:28:19 -0400

    Move this out of the JS

--- js/app.js
+++ js/app.js
@@ -1,10 +1,1 @@
-document.getElementById('base').innerHTML= `
-  <section id="main">
-    <div class="welcome">
-      <p>
-        Managed Assets Portal
-      </p>
-      <img src="resources/logo.png" class="welcomeLogo" alt="SES logo" />
-    </div>
-  </section>
-`
+// document.getElementById('base').innerHTML= ``
